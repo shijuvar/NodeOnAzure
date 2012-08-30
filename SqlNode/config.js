49f638f1f74b525e1c9c8490fd3674c7e0baf3fd
Shiju Varghese <shiju.varghese@live.com>
2012-08-30 17:56:48 +0530

    CRUD demo in Node.js/Express with Windows Azure SQL Database

---
+++ SqlNode/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "sqlExpressConnection": "Driver={SQL Server Native Client 10.0};Server=./SQLEXPRESS;Database=EFMVC;Trusted_Connection={Yes};",
+    "sqlAzureConnection" : "Driver={SQL Server Native Client 10.0};Server=tcp:{dbservername}.database.windows.net,1433;Database={database};Uid={username};Pwd={password};Encrypt=yes;Connection Timeout=30;"
+};
